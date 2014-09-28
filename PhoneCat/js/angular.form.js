08f7b83df33fe15d21bdece676831983158facaf
SiteDisks <peter@sitedisks.com.au>
2014-09-29 08:00:26 +1000

    Visual Studio management
    Form validation updates fo

--- PhoneCat/js/angular.form.js
+++ PhoneCat/js/angular.form.js
@@ -9,11 +9,30 @@ formApp.controller('formController', formController);
 
 function formController($scope) {
 
+    $scope.isRed = true;
+
+    $scope.changeColor = function() {
+        $scope.isRed = false;
+    }
+
     $scope.todos = [{ action: "Get groceries", complete: false },
         { action: "Call plumber", complete: false },
         { action: "Buy running shoes", complete: true },
         { action: "Buy flowers", complete: false },
         { action: "Call family", complete: false }];
+
+    $scope.locations = [{ name: 'Home' }, { name: 'Office' }, { name: 'Mall' }];
+
+    $scope.addNewItem = addNewItem;
+
+    function addNewItem(newTodo) {
+        $scope.todos.push(
+            {
+                action: newTodo.action + ' at ' + newTodo.location.name,
+                complete: false
+            });
+
+    }
 
 }
 
